bb5def8f166fe5b50e71998d56682747028bf16f
Gio Noa <gnoa001@fiu.edu>
2019-11-01 16:23:24 -0400

    Initial Commit

---
+++ Formula 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Formula 1
+//
+//  Created by Gio on 11/1/19.
+//  Copyright © 2019 Gio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
