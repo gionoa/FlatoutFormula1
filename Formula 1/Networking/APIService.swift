659a3790b6494bb3e0ed9adf8ea93066aec46535
Gio Noa <gnoa001@fiu.edu>
2019-11-02 19:40:44 -0400

    Refactor APIService

---
+++ Formula 1/Networking/APIService.swift
@@ -0,0 +1,48 @@
+//
+//  Publisher .swift
+//  Formula 1
+//
+//  Created by Gio on 11/1/19.
+//  Copyright © 2019 Gio. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+enum PublisherError: Error, CustomStringConvertible {
+    case network
+    case parsing
+    
+    var description: String {
+        switch self {
+            case .network: return "Failed to reach server"
+            case .parsing: return "Failed to decode from server"
+        }
+    }
+}
+
+struct APIService {
+    private static func dataTask(_ url: URL) -> AnyPublisher<Data, Error> {
+        URLSession.shared
+            .dataTaskPublisher(for: url)
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                    throw PublisherError.network
+                }
+                return data
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    static func fetch<T: Decodable>(url: String) -> AnyPublisher<T, PublisherError> {
+        let url = URL(string: url)!
+        
+        return
+            Self.dataTask(url)
+                .decode(type: T.self, decoder: JSONDecoder())
+                .mapError { error in
+                    return PublisherError.parsing
+                }
+                .eraseToAnyPublisher()
+    }
+}
