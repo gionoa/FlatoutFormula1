e06df62bdd000b5dabc2bde669badb6fa526b7d9
Giovanni Noa <self@gionoa.dev>
2019-11-05 15:55:39 -0500

    Add CollectionViewCell for Constructors

---
+++ Formula 1/Views/Cells/ConstructorCell.swift
@@ -0,0 +1,46 @@
+//
+//  ConstructorCollectionKitCell.swift
+//  Formula 1
+//
+//  Created by Gio on 11/5/19.
+//  Copyright © 2019 Gio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConstructorCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = String(describing: self)
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .darkGray
+        addSubviews()
+        activateConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        contentView.addSubview(titleLabel)
+    }
+    
+    func activateConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8)
+        ])
+    }
+    
+    func configure(title: String) {
+        titleLabel.text = title
+    }
+}
