12dfd5d53d3735371a998916a1cf727bb671b4df
Giovanni Noa <self@gionoa.dev>
2019-11-05 00:01:01 -0500

    Add DriverCell class for UIKit and SwiftUI cells

---
+++ Formula 1/Views/UIViews/Cells/DriverCell.swift
@@ -0,0 +1,76 @@
+//
+//  DriverCell.swift
+//  Formula 1
+//
+//  Created by Gio on 11/4/19.
+//  Copyright © 2019 Gio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+// MARK: - SwiftUI View
+struct DriverCell: View {
+    let firstName: String
+    let lastName: String
+    
+    var body: some View {
+        HStack {
+            Text(firstName)
+            Text(lastName)
+        }
+    }
+}
+
+// MARK: - UIKit View
+class DriverKitCell: UITableViewCell {
+    static let reuseIdentifier = String(describing: self)
+    
+    private lazy var firstNameLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var lastNameLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [firstNameLabel, lastNameLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubviews()
+        activateConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        contentView.addSubview(stackView)
+    }
+    
+    func activateConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8)
+        ])
+    }
+    
+    func configure(firstName: String, lastName: String) {
+        firstNameLabel.text = firstName
+        lastNameLabel.text = lastName
+    }
+}
