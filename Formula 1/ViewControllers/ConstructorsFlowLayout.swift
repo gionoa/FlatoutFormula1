2b916a17a031d745f267f527ad1f34cbbaa67c81
Giovanni Noa <self@gionoa.dev>
2019-11-11 17:53:31 -0500

    Add position value in constructor cell

--- Formula 1/ViewControllers/ConstructorsFlowLayout.swift
+++ Formula 1/ViewControllers/ConstructorsFlowLayout.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class ConstructorsFlowLayout: UICollectionViewFlowLayout {
     override func prepare() {
--- Formula 1/Views/Cells/ConstructorCell.swift
+++ Formula 1/Views/Cells/ConstructorCell.swift
@@ -12,6 +12,32 @@ import UIKit
 class ConstructorCollectionViewCell: UICollectionViewCell {
     static let reuseIdentifier = String(describing: self)
     
+    private lazy var positionTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.Formula1Font.Regular
+        label.adjustsFontForContentSizeCategory = true
+        label.text = "Position"
+        
+        return label
+    }()
+    
+    private lazy var positionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.Formula1Font.Bold
+        label.adjustsFontForContentSizeCategory = true
+        label.textColor = .systemRed
+        return label
+    }()
+    
+    private lazy var positionVStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [positionTitleLabel, positionLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        return  stackView
+    }()
+    
     // MARK: - Title (First HStack)
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
@@ -30,33 +56,33 @@ class ConstructorCollectionViewCell: UICollectionViewCell {
     private lazy var constructorHStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [titleLabel, nationalityLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-      //  stackView.alignment = .center
-        stackView.distribution = .fill
-        stackView.spacing = 8
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        stackView.spacing = 20
         return  stackView
     }()
     
     // MARK: - Points (VStack)
     private lazy var pointsTextLabel: UILabel = {
-           let label = UILabel()
-        label.font = UIFont.Formula1Font.Bold
+        let label = UILabel()
+        label.font = UIFont.Formula1Font.Regular
         label.adjustsFontForContentSizeCategory = true
-            label.text = "Points"
-           return label
-       }()
+        label.text = "Points"
+        return label
+    }()
     
     private lazy var pointsLabel: UILabel = {
         let label = UILabel()
         label.adjustsFontForContentSizeCategory = true
         label.font = UIFont.Formula1Font.Bold
         label.textColor = .systemRed
-
-      //  label.font = UIFont.preferredFont(forTextStyle: .title1).withSize(20)
+        
+        //  label.font = UIFont.preferredFont(forTextStyle: .title1).withSize(20)
         return label
     }()
     
-     private lazy var pointsStackView: UIStackView = {
+    private lazy var pointsStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [pointsTextLabel, pointsLabel])
         stackView.axis = .vertical
         stackView.alignment = .center
@@ -68,14 +94,14 @@ class ConstructorCollectionViewCell: UICollectionViewCell {
     // MARK: - Wins (VStack)
     private lazy var winsTextLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.Formula1Font.Bold
+        label.font = UIFont.Formula1Font.Regular
         label.text = "Wins"
         return label
     }()
     
     private lazy var winsLabel: UILabel = {
         let label = UILabel()
-      //  label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        //  label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
         label.font = UIFont.Formula1Font.Bold
         label.textColor = .systemRed
         return label
@@ -88,21 +114,22 @@ class ConstructorCollectionViewCell: UICollectionViewCell {
         stackView.distribution = .fill
         stackView.spacing = 8
         return stackView
-       }()
+    }()
     
     // MARK: - Stats (HStack of the VStacks)
     private lazy var statsStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [pointsStackView, winsStackView])
+        let stackView = UIStackView(arrangedSubviews: [ positionVStackView, pointsStackView, winsStackView])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
-       // stackView.alignment = .center
+        stackView.alignment = .center
         //stackView.distribution = .equalCentering
-        stackView.spacing = 40
+        stackView.spacing = 32
         return stackView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         addSubviews()
         activateConstraints()
         applySkin()
@@ -131,7 +158,7 @@ class ConstructorCollectionViewCell: UICollectionViewCell {
     
     func applySkin() {
         backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 0.15)
-
+        
         layer.cornerRadius = 20
     }
     
@@ -140,5 +167,6 @@ class ConstructorCollectionViewCell: UICollectionViewCell {
         pointsLabel.text = constructor.points
         winsLabel.text = constructor.wins
         nationalityLabel.text = constructor.constructor.nationality
+        positionLabel.text = constructor.position
     }
 }
