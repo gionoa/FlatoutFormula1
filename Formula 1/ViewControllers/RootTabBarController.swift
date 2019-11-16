9e5a3ba0862e8bb3f36b63eb72637472ecada579
Giovanni Noa <self@gionoa.dev>
2019-11-15 22:07:44 -0500

    Add custom navigation controller

--- Formula 1/ViewControllers/RootTabBarController.swift
+++ Formula 1/ViewControllers/RootTabBarController.swift
@@ -9,40 +9,30 @@
 import Foundation
 import UIKit
 
+private final class F1NavController: UINavigationController {
+    required init(rootViewController: UIViewController, titled title: String) {
+        super.init(rootViewController: rootViewController)
+        self.title = title
+        navigationBar.prefersLargeTitles = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 final class RootTabBarController: UITabBarController {
-    private let driversNavController: UINavigationController = {
-        let navController = UINavigationController(
-            rootViewController: DriversViewController())
-        
-        #warning("TODO: Handle Localized string")
-        navController.tabBarItem.title = "Drivers"
-        
-        navController.navigationBar.prefersLargeTitles = true
-        return navController
-    }()
+    #warning("TODO: Handle Localized string")
+    private let driversNavController = F1NavController(rootViewController: DriversViewController(),
+                                                       titled: "Drivers")
     
-    private let constructorsNavController: UINavigationController = {
-        let navController = UINavigationController(
-            rootViewController: ConstructorsViewController())
-        
-        #warning("TODO: Handle Localized string")
-        navController.tabBarItem.title = "Drivers"
-        
-        navController.navigationBar.prefersLargeTitles = true
-        return navController
-    }()
+    #warning("TODO: Handle Localized string")
+    private let constructorsNavController = F1NavController(rootViewController: ConstructorsViewController(),
+                                                            titled: "Constructors")
     
-    private let circuitsNavController: UINavigationController = {
-        let navController = UINavigationController(
-            rootViewController: CircuitsViewController())
-        
-        #warning("TODO: Handle Localized string")
-        navController.tabBarItem.title = "Drivers"
-        
-        navController.navigationBar.prefersLargeTitles = true
-        return navController
-    }()
-    
+    #warning("TODO: Handle Localized string")
+    private let circuitsNavController = F1NavController(rootViewController: DriversViewController(),
+                                                        titled: "Circuits")
     override func viewDidLoad() {
         #warning("TODO: Implement navigation")
         viewControllers = [driversNavController, constructorsNavController, circuitsNavController]
