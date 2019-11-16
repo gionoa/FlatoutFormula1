31d55afbea97c5fc382ac5fbabbfb8aec7c81c46
Giovanni Noa <self@gionoa.dev>
2019-11-16 00:30:21 -0500

    Change Table View implementation

--- Formula 1/ViewControllers/DriversViewController.swift
+++ Formula 1/ViewControllers/DriversViewController.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 // MARK: - Drivers View Controller
 #warning("Implement pull to refresh")
-final class DriversViewController: UITableViewController {
+final class DriversViewController: UIViewController {
     // MARK: - Properties
     private lazy var viewModel: DriversViewModel = {
         let viewModel = DriversViewModel()
@@ -19,9 +19,17 @@ final class DriversViewController: UITableViewController {
         return viewModel
     }()
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
     // MARK: init
     required init() {
-        super.init(style: .plain)
+        super.init(nibName: nil, bundle: nil)
         
         tableView.register(DriverKitCell.self,
                            forCellReuseIdentifier: DriverKitCell.reuseIdentifier)
@@ -32,35 +40,50 @@ final class DriversViewController: UITableViewController {
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
+        setupUI()
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
         
         #warning("TODO: Handle Localized string")
         navigationItem.title = "Drivers"
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 // MARK: - Table View Delegate / DataSource
-extension DriversViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { viewModel.numberOfDrivers }
+extension DriversViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { viewModel.numberOfDrivers }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let driver = viewModel.driver(at: indexPath.row)
-                
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: DriverKitCell.reuseIdentifier,
                                                  for: indexPath)
-                                                 as! DriverKitCell
+            as! DriverKitCell
         
         cell.configure(driver)
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension DriversViewController {
+    func setupUI() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
 
