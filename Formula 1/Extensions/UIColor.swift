86803cfddc5f6a44813b0c1b74e621ba40704f0a
Giovanni Noa <self@gionoa.dev>
2019-11-29 20:35:52 -0500

    Driver cell refactor (#13)
    * Add UIColor extension for constructor colors
    
    * Refactor Driver cell components
    
    * Finalize Driver UI

---
+++ Formula 1/Extensions/UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor.swift
+//  Formula 1
+//
+//  Created by Gio on 11/29/19.
+//  Copyright © 2019 Gio. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let Mercedes = UIColor(red: 0, green: 210 / 255, blue: 190 / 255, alpha: 1) // RGBA(0, 210, 190, 1)
+    static let Ferrari = UIColor(red: 220 / 255, green: 0, blue: 0, alpha: 1) // RGBA(220, 0, 0, 1)
+    static let Redbull = UIColor(red: 30 / 255, green: 65 / 255, blue: 255 / 255, alpha: 1) // RGBA(30, 65, 255, 1)
+    static let McLaren = UIColor(red: 255 / 255, green: 135 / 255, blue: 0, alpha: 1) // RGBA(255, 135, 0, 1)
+    static let Renault = UIColor(red: 255 / 255, green: 245 / 255, blue: 0, alpha: 1)//  RGBA(255, 245, 0, 1)
+    static let ToroRosso = UIColor(red: 70 / 255, green: 155 / 255, blue: 255 / 255, alpha: 1)// RGBA(70, 155, 255, 1)
+    static let RacingPoint = UIColor(red: 245 / 255, green: 150 / 255, blue: 200 / 255, alpha: 1)// RGBA(245, 150, 200, 1)
+    static let AlfaRomeo = UIColor(red: 155 / 255, green: 0, blue: 0, alpha: 1) // RGBA(155, 0, 0, 1)
+    static let Haas = UIColor(red: 189 / 255, green: 158 / 255, blue: 87 / 255, alpha: 1) // RGBA(189, 158, 87, 1)
+    static let Williams = UIColor(red: 0 / 255, green: 153 / 255, blue: 255 / 255, alpha: 1) // RGBA(0, 0, 0, 1)
+}
