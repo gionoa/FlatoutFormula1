78502b609da4e11cae3f564f83690f750b024cc1
gionoa <self@gionoa.dev>
2020-02-20 23:42:40 -0800

    Refactor Path to accept a season year

--- Formula 1/Networking/Endpoint.swift
+++ Formula 1/Networking/Endpoint.swift
@@ -13,19 +13,37 @@ enum Path: String {
     case circuits = "/current/circuits.json"
     case constructorStandings = "/current/constructorStandings.json"
     case drivers = "/current/driverStandings.json"
-    case driverStandings = "/driverStandings"
+    case driverStandings = "/driverStandings.json"
     case results = "/results"
     case raceStandings = "/current/results.json"
     case seasons = "/seasons.json"
+    
+    func path(for year: Int = 2019) -> String {
+        switch self {
+        case .circuits:
+            return "/\(year)/circuits.json"
+        case .constructorStandings:
+            return "/\(year)constructorStandings.json"
+        case .drivers:
+            return "/\(year)/driverStandings.json"
+        case .driverStandings:
+            return "/driverStandings.json"
+        case .results:
+            return "/results.json"
+        case .raceStandings:
+            return "/\(year)/results.json"
+        case .seasons:
+            return "/seasons.json"
+        }
+    }
 }
 
 // MARK: - Endpoint
 struct Endpoint {
     private (set) var path = "/api/f1"
     
-    init(_ subPath: Path) {
-        print(subPath.rawValue)
-        path.append(subPath.rawValue)
+    init(_ subPath: Path, for year: Int? = 2019) {
+        path.append(subPath.path(for: year ?? 2019))
     }
 }
 
